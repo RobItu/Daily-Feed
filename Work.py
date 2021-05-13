456ed9c17801087523e0b839f9f10db63669a421
RobItu <74835755+RobItu@users.noreply.github.com>
2021-05-13 13:58:57 -0600

    Integrated the weather for El Paso

--- Work.py
+++ Work.py
@@ -1,6 +1,8 @@
 import requests
 from pyowm.owm import OWM
 import pyowm
+#==========================================================================================================
+#Weather
 print('Good morning, Robert. \nHere is your Daily Feed for today:')
 print('The current weather is: ')
 owm = OWM('c5d07c67db6d86d9b0dcfd63fc9952bb')
@@ -10,6 +12,8 @@ EPH=-106.2005
 observation=mgr.weather_at_coords(EPL,EPH).weather
 temperature=observation.temperature('fahrenheit')
 print(temperature)
+#==========================================================================================================
+#Livin' Off the Land
 question1=input('Would you like to know a useful tip?\n' + 'Type y/n\n')
 if question1=='y':
     print('test1')
