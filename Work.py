f070591be1a810a606e974af7c0da1239695437d
RobItu <74835755+RobItu@users.noreply.github.com>
2021-05-13 13:50:44 -0600

    I added two lines of print.

--- Work.py
+++ Work.py
@@ -2,3 +2,10 @@ import requests
 import pyowm
 print('Good morning, Robert. \nHere is your Daily Feed for today:')
 print('The current weather is: ')
+owm = OWM('c5d07c67db6d86d9b0dcfd63fc9952bb')
+mgr=owm.weather_manager()
+EPL=31.8001
+EPH=-106.2005
+observation=mgr.weather_at_coords(EPL,EPH).weather
+temperature=observation.temperature('fahrenheit')
+print(temperature)
