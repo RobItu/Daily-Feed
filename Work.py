fff000977e7c55cfec6fbca0fdc16a509698c28f
RobItu <74835755+RobItu@users.noreply.github.com>
2021-05-12 23:30:14 -0600

    I added two lines of print.

--- Work.py
+++ Work.py
@@ -1,12 +1,8 @@
-import requests
+from pyowm.owm import OWM
 import pyowm
-print('Good morning, Robert. \nHere is your Daily Feed for today:')
-print('The current weather is: ')
-APIKEY= 'c5d07c67db6d86d9b0dcfd63fc9952bb'
-OpenWMap=pyowm.OWM(APIKEY)
-Weather=OpenWMap.weather_at_place('London')
-Data=Weather.get_weather()
-temp = Data.get_temperature(unit='celsius')      # get current temparature in celsius
-print ("Average Temp. Currently ", temp['temp']) # get avg. tmp
-print ("Max Temp. Currently ", temp['temp_max']) # get max tmp
-print ("Min Temp. Currently ", temp['temp_min']) # get min tmp>>
+
+owm=pyowm.OWM('c5d07c67db6d86d9b0dcfd63fc9952bb')
+obs=owm.weather_manager().weather_at_place('London, GB')
+
+print(w)
+
